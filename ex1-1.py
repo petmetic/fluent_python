bb5df661195e6685b16a5b16c79112a2df03980e
Meta <petmetic@gmail.com>
2023-07-21 12:27:54 +0200

    ex 1-1

--- ex1-1.py
+++ ex1-1.py
@@ -1,11 +1,13 @@
 import collections
+from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11) + list['JQKA']]
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
+
 
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
@@ -16,3 +18,10 @@ class FrenchDeck:
 
     def __getitem__(self, position):
         return self._cards[position]
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card, rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+breakpoint()
